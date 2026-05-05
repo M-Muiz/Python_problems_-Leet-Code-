5a78642b9c057ef97592d667e95e4827d2b04c6f
M-Muiz <mmuizsiddiqui@gmail.com>
2026-05-05 15:41:03 +0500

    work on boolean

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 # Basics
 
-print(2 + 3 * 6)
+# print(2 + 3 * 6)
+
+
+# a = 1
+# print('hello world', a)
+
+
+# print("What's your name?")
+# myname = input()
+# print("Good morning {}".format(myname))
+
+
+# print(len("tueday amir bahi laptop sell"))
+
+# print("hello" == "Hello")
+
+# print("hello" is "Hello")
+
+# print(True is True)
+# print(True is not True)
