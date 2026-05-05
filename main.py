157ce9af9a43078eb47ba0bbed03411042adf33c
M-Muiz <mmuizsiddiqui@gmail.com>
2026-05-05 16:06:27 +0500

    work on if/else

--- main.py
+++ main.py
@@ -20,3 +20,26 @@
 
 # print(True is True)
 # print(True is not True)
+
+
+# print(4 < 5 and 5 < 6)
+# print(4 < 5 and 5 > 6)
+
+
+# print(4 < 5 or 5 > 6)
+# print(4 > 5 or 5 > 6)
+
+
+# print(1 == 1 or 2 == 2)
+# print(1 != 1 or 2 != 2)
+
+
+# print(2 + 2 == 4 and not 2 + 2 == 5 and 2 * 2 == 2 + 2)
+
+
+# name = "Bobby"
+
+# if name == "alice":
+#     print("Hello", name)
+# else:
+#     print("Hello, stranger!")
